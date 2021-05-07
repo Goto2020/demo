57d1991d17212e9c3a7f163748963275188a963c
Goto2020 <53468985+Goto2020@users.noreply.github.com>
2021-05-07 21:30:02 +0800

    Add files via upload

---
+++ demo/src/test/java/javaTest.java
@@ -0,0 +1,18 @@
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class javaTest {
+    @Test
+    public void testcase1(){
+        Assert.assertFalse(false);
+        System.out.println("“testcase1”");
+    }
+    @Test
+    public void testcase2(){
+        Assert.assertTrue(true);
+        System.out.println("“testcase2”");
+    }
+}
